6a0524553a7ecb5104fc27ac4a11e207e43ed311
nuragiro67 <167646175+nuragiro67@users.noreply.github.com>
2024-10-16 14:28:01 +0600

    Add files via upload

--- project.student/js/home_works.js
+++ project.student/js/home_works.js
@@ -88,3 +88,41 @@ buttonAction()
 
 
 
+// dz 4.1
+
+const people = document.getElementById('people');
+const persons = new XMLHttpRequest();
+persons.open("GET", "../data/persons.json");
+persons.setRequestHeader("Content-type", "application/json");
+persons.onload = () => {
+    const data = JSON.parse(persons.responseText);
+    data.map(person => {
+        const card = document.createElement('div');
+        card.classList.add('person_card');
+        card.innerHTML = `
+         <h5>${person.name}</h5>
+         <img src="${person.photo}" alt="${person.name}" class="person_photo">
+         <p>Age: ${person.age}</p>
+       `;
+        people.appendChild(card);
+    });
+};
+
+persons.send();
+
+
+// dz 4.2
+
+const request = new XMLHttpRequest()
+// request.open('GET','../data/any.json')
+// request.setRequestHeader('Content-type', 'application/json')
+request.send()
+
+console.log(request)
+
+onload = () => {
+
+    let data = JSON.parse(request.responseText);
+
+    console.log(data);
+}
